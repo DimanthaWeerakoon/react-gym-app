cf01ecf5ac3d7212ba683691d5998fc7ca7e8dc9
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-02-22 23:40:57 +0530

    Routes added and paths intialized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetails from "./pages/ExerciseDetails";
+import Footer from "./components/Footer";
+
+import "./App.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetails />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
